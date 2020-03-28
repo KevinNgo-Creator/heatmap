70c5befc95a9b39eeed44a0a20ed43510b603b81
Kevin Ngo <ltn8rd@mail.umkc.edu>
2020-03-28 11:14:04 -0500

    push final adjust

---
+++ demo/heatmap.js
@@ -0,0 +1,9 @@
+var data = [{count: 2, date: "2017-09-23"}];
+
+var options = {months: 24};
+
+$("#heatmap-1").CalendarHeatmap( data , options );
+
+console.log($("#heatmap-1").CalendarHeatmap( data , options ));
+console.log(options)
+
